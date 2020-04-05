6b06a534551398768a2f63a46a4b7530a1fc5990
taketake8 <62596306+taketake8@users.noreply.github.com>
2020-04-05 20:04:25 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,11 @@ class UsersController < ApplicationController
   end
 
   def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
 
   end
 
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
 json.user_name @message.user.name
-json.created_at @message.created_at.to_i
+json.created_at @message.created_at.to_s
 json.content @message.content
 json.image @message.image_url
 
